cbfd71957ffbcd47cd2cdbd8246709570457d4d2
Julie Giunta <julie.giunta@epfl.ch>
2018-12-20 09:32:57 +0100

    Change example to handle outside params

--- examples/ListCompr.scala
+++ examples/ListCompr.scala
@@ -1,8 +1,12 @@
 object ListCompr{
+  def externalFunction(xs: L.List, t: Int): L.List = {
+    [2*x for x in L.concat(xs, xs) if x < t]
+  }
+
   val xs : L.List = L.Cons(1, L.Cons(2, L.Cons(3, L.Nil())));
   val ys : L.List = [ 2*x for x in L.concat(xs, xs) if !(x%2 == 0)];
-  val zs : L.List = [ 2*y for y in ys];
+  val zs : L.List = externalFunction(ys, 4);
   Std.printString(L.toString(ys));
-  Std.printString(L.toString(zs));
+  Std.printString(L.toString(zs))
 }
 
