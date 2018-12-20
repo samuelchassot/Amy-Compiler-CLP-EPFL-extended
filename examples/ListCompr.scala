2cf7c709b40bfe7c9af0abd6eec647c776fd4e76
Julie Giunta <julie.giunta@epfl.ch>
2018-12-20 09:41:17 +0100

    Change example to test hard cases

--- examples/ListCompr.scala
+++ examples/ListCompr.scala
@@ -4,9 +4,11 @@ object ListCompr{
   }
 
   val xs : L.List = L.Cons(1, L.Cons(2, L.Cons(3, L.Nil())));
-  val ys : L.List = [ 2*x for x in L.concat(xs, xs) if !(x%2 == 0)];
-  val zs : L.List = externalFunction(ys, 4);
+  val ys : L.List = [ y*x*z for x in L.concat(xs, xs) for y in xs for z in [a for a in xs] if !(x%2 == 0) && z == 1];
+  val zs : L.List = externalFunction(ys, 5);
+  val as : L.List = [z for z in zs];
   Std.printString(L.toString(ys));
-  Std.printString(L.toString(zs))
+  Std.printString(L.toString(zs));
+  Std.printString(L.toString(as))
 }
 
